5a9e84c33c97ec95e839236bfca51d838df34db5
Sebastien Mesili <leseb77@hotmail.com>
2022-04-12 22:11:46 +0900

    Working example

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+     <div className="navbar-container">
+      <div className="navbar">
+        <a className="item home">
+            A
         </a>
-      </header>
+        <a className="item user">
+            B
+        </a>
+        <a className="item bag active">
+            C
+        </a>
+        <a className="item phone">
+            D
+        </a>
+        <a className="item search">
+            E
+        </a>
+      </div>
+      </div>
     </div>
   );
 }
